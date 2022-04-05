f156f50d28258081ea326403b48d33c16c2e8f14
Mumuni Tahiru <peptahiru@gmail.com>
2022-04-05 08:46:35 +0000

    beginning

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+int main(){
+    int computer_choice,
+        player_choice,
+        computer_total=0,
+        player_total=0;
+    //defining random number for computer choice
+    srand(time(0));
+    
+
+
+
+    cout<<"To play this game you are to enter 1 for Rock, 2 for Paper and 3 for Scissors\n";
+
+
+    
+}
