401f37f28c93e54b422b97a91bddf0bb1d324d34
Mumuni Tahiru <peptahiru@gmail.com>
2022-04-08 17:39:46 +0000

    still in progress

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<cstdlib>
 #include<string>
 using namespace std;
-const spring choice[4]={" ", "Rock", "Paper", "Scissors"}
+const string choice[4]={" ", "Rock", "Paper", "Scissors"};
 int main(){
     int computer_choice,
         player_choice,
@@ -16,7 +16,7 @@ do
 {
     //defining computer_choice
     unsigned seed=time(0);
-    srand(seed)
+    srand(seed);
     computer_choice=rand()%3+1;
     cout<<"make your choice 1(rock) 2(paper) 3(scissors)"<<endl;
     cin>>player_choice;
@@ -28,14 +28,27 @@ do
     else if((computer_choice==1 && player_choice==2)||(computer_choice==2 && player_choice==3)||(computer_choice==3 && player_choice==1))
     {
         cout<<"you won!"<<endl;
-        cout<<"you chose"<<choice[player_choice]<<"which beats what I chose "<<choice[computer_choice]<<ebndl;
+        cout<<"you chose"<<choice[player_choice]<<"which beats what I chose "<<choice[computer_choice]<<endl;
+        player_total++;
     }
     else
     {
+        cout<<"I win!\n";
+        cout<<"I chose "<<choice[computer_choice]<<"which beats what you chose "<<choice[player_choice]<<endl;
+        computer_total++;
     }
 } while (computer_total<5 || player_total<5);
+cout<<"Total points won by me "<<computer_total<<"compared to total points won by you "<<player_total<<endl;
+if(computer_total>player_total)
+{
+    cout<<"I am the champion!"<<endl;
+}
+else
+{
+    cout<<"you are the champion!\n";
+}
 
 
 
-    
+   return 0; 
 }
